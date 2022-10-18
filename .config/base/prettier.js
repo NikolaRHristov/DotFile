bf5857ab420b561b01afe7ede42582bac4f1c67b
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-18 15:30:40 +0300

    Cleanup

---
+++ .config/base/prettier.js
@@ -0,0 +1,84 @@
+const MODULES_DIR = "C:\\Users\\nhris\\AppData\\Roaming\\npm\\node_modules\\";
+
+module.exports = {
+	editorConfig: true,
+	arrowParens: "always",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	cursorOffset: -1,
+	embeddedLanguageFormatting: "auto",
+	endOfLine: "lf",
+	htmlWhitespaceSensitivity: "css",
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: "always",
+	quoteProps: "preserve",
+	requirePragma: false,
+	semi: true,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "es5",
+	useTabs: true,
+	vueIndentScriptAndStyle: true,
+	plugins: [
+		MODULES_DIR + "prettier-plugin-tailwindcss",
+		MODULES_DIR + "@prettier/plugin-php",
+		MODULES_DIR + "@prettier/plugin-lua",
+		MODULES_DIR + "@prettier/plugin-php",
+		MODULES_DIR + "prettier-plugin-astro",
+		MODULES_DIR + "prettier-plugin-java",
+		MODULES_DIR + "prettier-plugin-kotlin",
+		MODULES_DIR + "prettier-plugin-pkg",
+		MODULES_DIR + "prettier-plugin-sh",
+		MODULES_DIR + "prettier-plugin-svelte",
+		MODULES_DIR + "prettier-plugin-toml",
+		MODULES_DIR + "@trivago/prettier-plugin-sort-imports",
+	],
+	tailwindConfig: "",
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+		{
+			files: "*.svelte",
+			options: {
+				parser: "svelte",
+			},
+		},
+		{
+			files: "*.lua",
+			options: {
+				parser: "lua",
+			},
+		},
+		{
+			files: "*.toml",
+			options: {
+				parser: "toml",
+			},
+		},
+		{
+			files: "package.json",
+			options: {
+				trailingComma: "none",
+			},
+		},
+	],
+	svelteSortOrder: "options-scripts-styles-markup",
+	svelteStrictMode: true,
+	svelteAllowShorthand: true,
+	svelteIndentScriptAndStyle: true,
+	importOrder: [
+		"^@(.*)/(.*)$",
+		"^@core/(.*)$",
+		"^@server/(.*)$",
+		"^@ui/(.*)$",
+		"^[./]",
+	],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+};
