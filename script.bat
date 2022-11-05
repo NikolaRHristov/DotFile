7cef0d6b768ce2d00042fa6387ab6406b910cff0
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-05 19:35:18 +0200

    squash!

---
+++ script.bat
@@ -0,0 +1,27 @@
+rm C:\Users\nhris\.aliases
+rm C:\Users\nhris\.bash_profile
+rm C:\Users\nhris\.bashrc
+rm C:\Users\nhris\.config
+rm C:\Users\nhris\.functions
+rm C:\Users\nhris\.gitconfig
+rm C:\Users\nhris\.oh-my-bash
+rm C:\Users\nhris\.oh-my-posh
+rm C:\Users\nhris\.oh-my-zsh
+rm C:\Users\nhris\.ssh
+rm C:\Users\nhris\prettier.config.js
+rm C:\Users\nhris\rustfmt.toml
+rm C:\Users\nhris\tailwind.config.js
+
+mklink C:\Users\nhris\.aliases D:\Developer\app\nikolaxhristov\dot\.aliases
+mklink C:\Users\nhris\.bash_profile D:\Developer\app\nikolaxhristov\dot\.bash_profile
+mklink C:\Users\nhris\.bashrc D:\Developer\app\nikolaxhristov\dot\.bashrc
+mklink /D C:\Users\nhris\.config D:\Developer\app\nikolaxhristov\dot\.config
+mklink C:\Users\nhris\.functions D:\Developer\app\nikolaxhristov\dot\.functions
+mklink C:\Users\nhris\.gitconfig D:\Developer\app\nikolaxhristov\dot\.gitconfig
+mklink /D C:\Users\nhris\.oh-my-bash D:\Developer\app\nikolaxhristov\dot\.oh-my-bash
+mklink /D C:\Users\nhris\.oh-my-posh D:\Developer\app\nikolaxhristov\dot\.oh-my-posh
+mklink /D C:\Users\nhris\.oh-my-zsh D:\Developer\app\nikolaxhristov\dot\.oh-my-zsh
+mklink /D C:\Users\nhris\.ssh E:\MEGA\PROJECTS\Personal\Credentials\Keys\SSH
+mklink C:\Users\nhris\prettier.config.js D:\Developer\app\nikolaxhristov\dot\prettier.config.js
+mklink C:\Users\nhris\rustfmt.toml D:\Developer\app\nikolaxhristov\dot\rustfmt.toml
+mklink C:\Users\nhris\tailwind.config.js D:\Developer\app\nikolaxhristov\dot\tailwind.config.js
