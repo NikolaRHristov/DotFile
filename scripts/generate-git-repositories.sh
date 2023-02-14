e99d951afddb6ad8f7bafb6ac32d9b406074edf6
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-14 19:03:55 +0200

    squash!

---
+++ scripts/generate-git-repositories.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DEVDIR="/d/Developer/app"
+
+USERS=(
+	"nikolaxhristov"
+)
+
+for USER in "${USERS[@]}"; do
+	USER=$(echo "$USER" | tr -d '\"')
+	readarray -t REPOS < <(gh api users/"$USER"/repos | jq .[].ssh_url)
+	readarray -t ORGS < <(gh api users/"$USER"/orgs | jq .[].login)
+
+	echo "Cloning all for user: $USER"
+	mkdir -p $DEVDIR/"$USER"
+	cd $DEVDIR/"$USER" || exit
+
+	for REPO in "${REPOS[@]}"; do
+		REPO=$(echo "$REPO" | sed "s/git@github\.com\:/ssh\:\/\/git\@github\.com\//" | tr -d '\"')
+
+		REPONAME="${REPO/.git/}"
+		REPONAME="${REPONAME/ssh:\/\/git@github.com\//}"
+		mkdir $DEVDIR/"$REPONAME"
+		cd $DEVDIR/"$REPONAME" || exit
+
+		git init
+		git remote add origin "$REPO"
+	done
+
+	cd - || exit
+
+	for ORG in "${ORGS[@]}"; do
+		ORG=$(echo "$ORG" | tr -d '\"')
+		readarray -t REPOS < <(gh api orgs/"$ORG"/repos | jq .[].ssh_url)
+
+		echo "Cloning all for org: $ORG"
+		mkdir -p $DEVDIR/"$ORG"
+		cd $DEVDIR/"$ORG" || exit
+
+		for REPO in "${REPOS[@]}"; do
+			REPO=$(echo "$REPO" | sed "s/git@github\.com\:/ssh\:\/\/git\@github\.com\//" | tr -d '\"')
+
+			REPONAME="${REPO/.git/}"
+			REPONAME="${REPONAME/ssh:\/\/git@github.com\//}"
+			mkdir $DEVDIR/"$REPONAME"
+			cd $DEVDIR/"$REPONAME" || exit
+
+			git init
+			git remote add origin "$REPO"
+		done
+
+		cd - || exit
+	done
+done
---
+++ scripts/sync-git-repositories.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DEVDIR="/d/Developer/app"
+
+USERS=(
+	"nikolaxhristov"
+)
+
+for USER in "${USERS[@]}"; do
+	USER=$(echo "$USER" | tr -d '\"')
+	readarray -t REPOS < <(gh api users/"$USER"/repos | jq .[].ssh_url)
+	readarray -t ORGS < <(gh api users/"$USER"/orgs | jq .[].login)
+
+	echo "Cloning all for user: $USER"
+	mkdir -p $DEVDIR/"$USER"
+	cd $DEVDIR/"$USER" || exit
+
+	for REPO in "${REPOS[@]}"; do
+		REPO=$(echo "$REPO" | sed "s/git@github\.com\:/ssh\:\/\/git\@github\.com\//" | tr -d '\"')
+
+		git clone --recurse-submodules "$REPO"
+	done
+
+	cd - || exit
+
+	for ORG in "${ORGS[@]}"; do
+		ORG=$(echo "$ORG" | tr -d '\"')
+		readarray -t REPOS < <(gh api orgs/"$ORG"/repos | jq .[].ssh_url)
+
+		echo "Cloning all for org: $ORG"
+		mkdir -p $DEVDIR/"$ORG"
+		cd $DEVDIR/"$ORG" || exit
+
+		for REPO in "${REPOS[@]}"; do
+			REPO=$(echo "$REPO" | sed "s/git@github\.com\:/ssh\:\/\/git\@github\.com\//" | tr -d '\"')
+
+			if [[ "$ORG" == "astro-community" ]]; then
+				if [[ "$REPO" =~ .*astro-critters.* ]] || [[ "$REPO" =~ .*astro-compress.* ]] || [[ "$REPO" =~ .*github-actions-runner.* ]] || [[ "$REPO" =~ .*astro-rome.* ]]; then
+					git clone --recurse-submodules "$REPO"
+				fi
+			else
+				if [[ "$ORG" == "playform" ]]; then
+					if ! [[ "$REPO" =~ .*aws.* ]] && ! [[ "$REPO" =~ .*ec2.* ]]; then
+						git clone --recurse-submodules "$REPO"
+					fi
+				else
+					git clone --recurse-submodules "$REPO"
+				fi
+			fi
+		done
+
+		cd - || exit
+	done
+done
