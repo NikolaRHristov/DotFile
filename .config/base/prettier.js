e7285b17d8a708838499e1ec63a6c526c84bf4d0
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-14 02:33:59 +0300

    squash!

--- .config/base/prettier.js
+++ .config/base/prettier.js
@@ -32,11 +32,12 @@ module.exports = {
 		// `${MODULES_DIR}prettier-plugin-java`,
 		// `${MODULES_DIR}prettier-plugin-kotlin`,
 		// `${MODULES_DIR}prettier-plugin-organize-attributes`,
-		// `${MODULES_DIR}prettier-plugin-pkg`,
-		// `${MODULES_DIR}prettier-plugin-sh`,
+		`${MODULES_DIR}prettier-plugin-autocorrect`,
+		`${MODULES_DIR}prettier-plugin-pkg`,
+		`${MODULES_DIR}prettier-plugin-sh`,
 		// @TODO: Check for support
 		// `${MODULES_DIR}prettier-plugin-sort-imports`,
-		// `${MODULES_DIR}prettier-plugin-sql`,
+		`${MODULES_DIR}prettier-plugin-sql`,
 		// `${MODULES_DIR}prettier-plugin-svelte`,
 		// @TODO: Check for support
 		// `${MODULES_DIR}prettier-plugin-tailwindcss`,
