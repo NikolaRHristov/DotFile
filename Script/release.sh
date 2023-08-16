3e3d4a6bce186ec188a314135ddb8019b8368054
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-16 14:29:12 +0300

    squash!

--- Script/release.sh
+++ Script/release.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-VERSION="x" ; clean_dependencies ; bump_dependencies ; dum install ; dum prepublishOnly ; git add . ; git commit -m $VERSION ; git sync ; git tag -d "v${VERSION}" ; git push --delete origin "v${VERSION}" ; git tag -s -m "v${VERSION}" -a "v${VERSION}" ; git push --tags ; gh release delete "v${VERSION}" -y ; gh release create "v${VERSION}" --generate-notes ; 
+VERSION="x"
+clean_dependencies
+bump_dependencies
+dum install
+dum prepublishOnly
+git add .
+git commit -m $VERSION
+git sync
+git tag -d "v${VERSION}"
+git push --delete origin "v${VERSION}"
+git tag -s -m "v${VERSION}" -a "v${VERSION}"
+git push --tags
+gh release delete "v${VERSION}" -y
+gh release create "v${VERSION}" --generate-notes
