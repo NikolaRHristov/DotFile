7dd60a91ded39ce94b8651f055e7597c6b5bed9c
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-08 20:30:51 +0300

    squash!

--- Script/release.sh
+++ Script/release.sh
@@ -2,11 +2,11 @@
 
 # Cargo
 
-# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; cargo build --release ; Maintan Workflow ; git add . ; git commit -m "$VERSION" ; git sync ; git tag -d "${VERSION}" ; git push --delete origin "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes ; cargo publish ;
+# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; cargo build --release ; Maintan Workflow ; git add . ; git commit -m "$VERSION" ; git sync ; git tag -d "${VERSION}" ; git push --delete Source "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes ; cargo publish ;
 
 # NPM
 
-# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; dum install ; dum prepublishOnly ; dum Document ; Maintain Workflow ; git add . ; git commit -m "${VERSION}" ; git sync ; git tag -d "${VERSION}" ; git push --delete origin "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes
+# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; dum install ; dum prepublishOnly ; dum Document ; Maintain Workflow ; git add . ; git commit -m "${VERSION}" ; git sync ; git tag -d "${VERSION}" ; git push --delete Source "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes
 
 VERSION="x"
 clean_dependencies
@@ -18,7 +18,7 @@ git add .
 git commit -m $VERSION
 git sync
 git tag -d "v${VERSION}"
-git push --delete origin "v${VERSION}"
+git push --delete Source "v${VERSION}"
 git tag -s -m "v${VERSION}" -a "v${VERSION}"
 git push --tags
 gh release delete "v${VERSION}" -y
