879ff82d7964edbb66f082b88ee2ea462fd05113
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-16 23:28:33 +0300

    squash!

--- Script/commit-and-fetch.sh
+++ Script/commit-and-fetch.sh
@@ -3,14 +3,14 @@
 cd /d/Developer || exit
 
 PRun -p -f .git git add .
-PRun -p -f .git git commit -m squash!
+PRun -p -f .git git ecommit -m squash!
 PRun -p -f .git git sync
 PRun -p -f .git git fetch upstream
 PRun -p -f .git git merge upstream/main
 PRun -p -f .git git sync
 
 PRun -p .git git add .
-PRun -p .git git commit -m squash!
+PRun -p .git git ecommit -m squash!
 PRun -p .git git sync
 PRun -p .git git fetch upstream
 PRun -p .git git merge upstream/main
--- Script/release.sh
+++ Script/release.sh
@@ -2,11 +2,11 @@
 
 # Cargo
 
-# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; cargo build --release ; Maintan Workflow ; git add . ; git commit -m "$VERSION" ; git sync ; git tag -d "${VERSION}" ; git push --delete Source "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes ; cargo publish ;
+# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; cargo build --release ; Maintan Workflow ; git add . ; git ecommit -m "$VERSION" ; git sync ; git tag -d "${VERSION}" ; git push --delete Source "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes ; cargo publish ;
 
 # NPM
 
-# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; dum install ; dum prepublishOnly ; Document 'Source/**/*.ts' ; Maintain Workflow ; git add . ; git commit -m "${VERSION}" ; git sync ; git tag -d "${VERSION}" ; git push --delete Source "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes
+# VERSION="Name/v0.0.0" ; clean_dependencies ; bump_dependencies ; dum install ; dum prepublishOnly ; Document 'Source/**/*.ts' ; Maintain Workflow ; git add . ; git ecommit -m "${VERSION}" ; git sync ; git tag -d "${VERSION}" ; git push --delete Source "${VERSION}" ; git tag -s -m "${VERSION}" -a "${VERSION}" ; git push --tags ; gh release delete "${VERSION}" -y ; gh release create "${VERSION}" --generate-notes
 
 VERSION="x"
 clean_dependencies
@@ -15,7 +15,7 @@ dum install
 dum prepublishOnly
 dum Document
 git add .
-git commit -m $VERSION
+git ecommit -m $VERSION
 git sync
 git tag -d "v${VERSION}"
 git push --delete Source "v${VERSION}"
