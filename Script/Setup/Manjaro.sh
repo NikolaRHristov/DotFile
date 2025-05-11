68c84eb8caa3fa24ad30ea00ef4b241bacf1ee39
Nikola Hristov <Nikola@PlayForm.Cloud>
2025-05-11 12:56:02 +0300

    Changes

--- Script/Setup/Manjaro.sh
+++ Script/Setup/Manjaro.sh
@@ -3,11 +3,27 @@
 Current=$(\cd -- "$(\dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && \pwd)
 
 # shellcheck disable=SC1091
-source "$Current/Fn/Remove.sh"
+# source "$Current/Fn/Remove.sh"
 
 DOTFILE="/home/nikola/Developer/Application/NikolaRHristov/DotFile/"
 
-mkdir -p ~/.config
+mv ~/.gitconfig ~/.gitconfig.bak
+mv ~/.aliases ~/.aliases.bak
+mv ~/.bash_history_shared ~/.bash_history_shared.bak
+mv ~/.bash_logout ~/.bash_logout.bak
+mv ~/.bash_profile ~/.bash_profile.bak
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.functions ~/.functions.bak
+mv ~/.gitmessage ~/.gitmessage.bak
+mv ~/.zshrc ~/.zshrc.bak
+mv ~/Bash ~/Bash.bak
+mv ~/PowerShell ~/PowerShell.bak
+mv ~/ZSH ~/ZSH.bak
+mv ~/biome.json ~/biome.json.bak
+mv ~/prettier.config.js ~/prettier.config.js.bak
+mv ~/rome.json ~/rome.json.bak
+mv ~/rustfmt.toml ~/rustfmt.toml.bak
+mv ~/tailwind.config.js ~/tailwind.config.js.bak
 
 cp "$DOTFILE".gitconfig ~/.gitconfig
 ln -s "$DOTFILE".aliases ~/.aliases
@@ -21,11 +37,11 @@ ln -s "$DOTFILE".zshrc ~/.zshrc
 ln -s "$DOTFILE"Bash ~/Bash
 ln -s "$DOTFILE"PowerShell ~/PowerShell
 ln -s "$DOTFILE"ZSH ~/ZSH
-ln -s "$DOTFILE"biome.json ~/biome.json
-ln -s "$DOTFILE"prettier.config.js ~/prettier.config.js
-ln -s "$DOTFILE"rome.json ~/rome.json
-ln -s "$DOTFILE"rustfmt.toml ~/rustfmt.toml
-ln -s "$DOTFILE"tailwind.config.js ~/tailwind.config.js
+ln -s "$DOTFILE"Configuration/biome.json ~/biome.json
+ln -s "$DOTFILE"Configuration/prettier.config.mjs ~/prettier.config.js
+ln -s "$DOTFILE"Configuration/rome.json ~/rome.json
+ln -s "$DOTFILE"Configuration/rustfmt.toml ~/rustfmt.toml
+ln -s "$DOTFILE"Configuration/tailwind.config.js ~/tailwind.config.js
 
 # rm ~/Developer/.clang-format
 # rm ~/Developer/.csharpierrc
